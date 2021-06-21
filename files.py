34a411c801d21e7571efec907b4dbb88053dee67
Cody Siegmann <codysiegmann09@gmail.com>
2021-06-21 13:40:27 -0700

    Finished files

---
+++ files.py
@@ -0,0 +1,15 @@
+# f = open('demo.txt', mode='r')
+# # f.write('Add this content!\n')
+# line = f.readline()
+# while(line):
+#     print(line)
+#     line = f.readline()[:-1]
+# f.close()
+
+# with open('demo.txt', mode='r') as f:    
+#     line = f.readline()
+#     while(line):
+#         print(line)
+#         line = f.readline()
+# user_input = input('testing')
+# print('done')
